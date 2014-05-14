d10992bb0477e26ce99872e1db818dfce4072e96
Alison Knappik <alison.knappik@d214.org>
2014-05-14 13:19:02 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Alison K. Knappik on 5/14/14.
+//  Copyright (c) 2014 aknapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Alison K. Knappik on 5/14/14.
+//  Copyright (c) 2014 aknapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
